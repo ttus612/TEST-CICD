4ac2e32031d567873965a1d54637946dd38d87ee
ttus612 <thanhtu6122002@gmail.com>
2024-04-09 17:18:46 +0700

    Fix unit test

--- src/test/java/com/example/testcicd/TestCicdApplicationTests.java
+++ src/test/java/com/example/testcicd/TestCicdApplicationTests.java
@@ -14,52 +14,52 @@ import org.springframework.test.annotation.Rollback;
 import java.util.List;
 
 @SpringBootTest
-//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class TestCicdApplicationTests {
-//    @Autowired
-//    private ProductRepository productRepository;
-//
-//
-//    @Test
-//    @Order(1)
-//    @Rollback(value = false)
-//    void contextLoads() {
-//        System.out.println("Start server successfully");
-//
-//    }
-//
-//    @Test
-//    @Order(2)
-//    @Rollback(value = false)
-//    public void testSaveProduct() {
-//        Product product = Product.builder().name("Test Product").build();
-//        productRepository.save(product);
-//        Assertions.assertThat(product.getId()).isGreaterThan(0);
-//    }
-//
-//    @Test
-//    @Order(3)
-//    @Rollback(value = false)
-//    public void testGetAllProduct() {
-//        List<Product> products = productRepository.findAll();
-//        Assertions.assertThat(products).size().isGreaterThan(0);
-//    }
-//
-//    @Test
-//    @Order(4)
-//    @Rollback(value = false)
-//    public void testGetOneProduct() {
-//        Product product = productRepository.findById(2L).get();
-//        Assertions.assertThat(product.getId()).isEqualTo(2L);
-//    }
-//
-//    @Test
-//    @Order(5)
-//    @Rollback(value = false)
-//    public void testDeleteProduct() {
-//        productRepository.deleteById(1L);
-//        Product product = productRepository.findById(1L).orElse(null);
-//        Assertions.assertThat(product).isNull();
-//    }
+    @Autowired
+    private ProductRepository productRepository;
+
+
+    @Test
+    @Order(1)
+    @Rollback(value = false)
+    void contextLoads() {
+        System.out.println("Start server successfully");
+
+    }
+
+    @Test
+    @Order(2)
+    @Rollback(value = false)
+    public void testSaveProduct() {
+        Product product = Product.builder().name("Test Product").build();
+        productRepository.save(product);
+        Assertions.assertThat(product.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    @Order(3)
+    @Rollback(value = false)
+    public void testGetAllProduct() {
+        List<Product> products = productRepository.findAll();
+        Assertions.assertThat(products).size().isGreaterThan(0);
+    }
+
+    @Test
+    @Order(4)
+    @Rollback(value = false)
+    public void testGetOneProduct() {
+        Product product = productRepository.findById(2L).get();
+        Assertions.assertThat(product.getId()).isEqualTo(2L);
+    }
+
+    @Test
+    @Order(5)
+    @Rollback(value = false)
+    public void testDeleteProduct() {
+        productRepository.deleteById(1L);
+        Product product = productRepository.findById(1L).orElse(null);
+        Assertions.assertThat(product).isNull();
+    }
 
 }
