612cf4cfed7f3f986a649711a08d6c41b007ca74
ttus612 <thanhtu6122002@gmail.com>
2024-04-10 06:30:39 +0700

    commit }

--- src/main/java/com/example/testcicd/controller/ProductController.java
+++ src/main/java/com/example/testcicd/controller/ProductController.java
@@ -23,7 +23,7 @@ public class ProductController {
     @GetMapping("/products")
     public List<Product> getAllProduct(){
         return productRepository.findAll();
-    }
+    }]
 
 
     @GetMapping("/product/{id}")
